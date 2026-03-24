691de61a5034e36a492ed622e283abbcb782839e
SurajPawarDev15 <supersuraj143@gmail.com>
2026-03-24 08:40:15 +0530

    added the login component

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Topbar } from './components/topbar/topbar';
+import { MatButtonModule } from '@angular/material/button';
+import { Login } from './pages/login/login';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [App, Topbar],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [App, Topbar, Login],
+  imports: [BrowserModule, AppRoutingModule, MatButtonModule, MatFormFieldModule, MatInputModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
